cdefc9269468e61d0d8a80107dbc208ffd8f4eda
Steve Alsop <me@stevenalsop.com>
2021-04-23 23:05:08 +0100

    added template back in

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "panos_panorama_device_group" "this" {
   name = "test-commit"
 }
 
-//resource "panos_panorama_template_stack" "this" {
-//  name = "test-commit"
-//}
+resource "panos_panorama_template_stack" "this" {
+  name = "test-commit"
+}
 
 # =====================================================================================================================
 #   Panorama Commit
@@ -23,6 +23,6 @@ module "commit" {
   # List all PANOS Objects as dependencies to ensure commit executes after all resources have been created.
   depends_on = [
     panos_panorama_device_group.this,
-//    panos_panorama_template_stack.this,
+    panos_panorama_template_stack.this,
   ]
 }
